492d38909f2c784d4c3af22dac256fde9248b923
Ivan Yu <gun123kk@google.com>
2021-08-02 11:48:02 +0800

    update build_exe.sh for building exe while running exe with/without console method
    below command will be created an exe that without consloe while user run it(recommended)
    pyinstaller -F --noconsole --onefile ./main.py
    
    below command will be created an exe that with consloe while user run it(for bebuging)
    pyinstaller -F ./main.py

--- build_exe.sh
+++ build_exe.sh
@@ -3,7 +3,11 @@
 # pip install pyinstaller
 # note 2: chmod +x build_exe.sh
 # ubuntu
-pyinstaller -F ./main.py
+# below command will be created an exe that without consloe while user run it
+pyinstaller -F --noconsole --onefile ./main.py
+#pyinstaller -F --onefile main.py --windowed
+# below command will be created an exe that with consloe while user run it
+#pyinstaller -F ./main.py
 cp ./dist/main ./
 mv main vott_retain_json.exe
 rm -rf dist
